07b9029f360d285caa6b56b28943bb64f8c7aece
Pedro Boueke <phboueke@gmail.com>
2016-04-24 20:16:22 -0300

    signal error handling

--- proj1/signals/pt1/main.cpp
+++ proj1/signals/pt1/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <signal.h>
+#include <string.h>
 
 using namespace std;
 
+// process, signal
 int main(int argc, char* argv[]) {
     try
     {
-        kill( atoi(argv[1]), atoi(argv[2]));
-        printf( "Sent signal %s to process %s", argv[1], argv[2]);
+        int status = kill( atoi(argv[1]), atoi(argv[2]));
+        if (status == 0){
+            printf( "Sent signal %s to process %s", argv[1], argv[2]);
+        }
+        else if (status == -1) {
+            printf("Status[%d], Error[%d]: %s", status, errno, strerror(errno));
+        }
+        else
+        {
+            printf("Status[%d], unidentified path", status);
+        }
     }
     catch (int e) {
-        printf ("Error: unable to send signal...");
+        printf ("Error: unable to send signal... %d", e);
     }
     return 0;
 }

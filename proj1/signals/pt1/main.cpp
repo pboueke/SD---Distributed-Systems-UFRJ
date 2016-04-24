5a97ff23f1cbfdb0ff4f89f7fcc34a226c6733df
Pedro Boueke <phboueke@gmail.com>
2016-04-24 19:21:55 -0300

    Sending signals

---
+++ proj1/signals/pt1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <signal.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    kill( atoi(argv[1]), atoi(argv[2]));
+    printf( "Sent signal %d to process %d", argv[1], argv[2]);
+}

2345e8d8a64eeee68df87ec5be7f33ebec798fd2
Erik Tronkos <erik.tronkos@poli.ufrj.br>
2016-04-24 19:48:11 -0300

    programa 2 feito

--- proj1/signals/pt1/program1.cpp
+++ proj1/signals/pt1/program1.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// program pid signal
 int main(int argc, char* argv[]) {
     kill( atoi(argv[1]), atoi(argv[2]));
     printf( "Sent signal %d to process %d", argv[1], argv[2]);
